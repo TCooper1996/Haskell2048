cb6afe4e23eb2dbe9b7733debacf08cd9af24ce5
taylercooper <trcooper1996@gmail.com>
2022-07-18 19:26:36 -0400

    Monadic Form

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Main where
 import Data.List (transpose, intersperse)
 import Data.Function(on)
@@ -6,9 +7,11 @@ import Data.Maybe
 import System.Random
 
 import Control.Applicative (Applicative(liftA2), Alternative ((<|>)))
-import Control.Monad.State (State, StateT (StateT), state)
+import Control.Monad.State (State, StateT (StateT), state, MonadTrans, MonadState)
 import Data.Bits (Bits(shift))
 import Control.Monad (ap, liftM)
+import Control.Monad.Trans (MonadTrans(lift))
+import Control.Monad.RWS (MonadState(get, put))
 
 data Grid a = Grid {getGrid :: [[a]]}
 type Tiles = [Tile]
@@ -27,14 +30,39 @@ type Rows = [Tiles]
 type Columns = [Tiles]
 
 --data GState a = GState {getGrid1 :: Grid a, getGen ::  StdGen}
-data GState a = GState {getGrid1 :: a, getGen ::  StdGen}
+data GState a = GState {getVal :: a, getGen ::  StdGen}
 instance Functor GState where
     fmap f (GState g gen) = GState (f g) gen
 
-newtype GameState s = GameState {getState :: s}
---newtype GameState s a = GameState {runState :: s -> (a,s)}
+--newtype GameState s = GameState {getState :: s}
+newtype GameState s a = GameState {runState :: s -> (a, s)}
 
-newtype MState s a = MState {runState2 :: s -> (a,s)}
+newtype StateIO s m a= StateIO {runStateT :: (s -> m (a,s))}
+
+instance (Monad m) => Functor (StateIO s m) where
+    fmap = liftM
+
+instance (Monad m) => Applicative (StateIO s m) where
+    pure = return
+    (<*>) = ap
+
+instance (Monad m) => MonadState s (StateIO s m) where
+    get = StateIO $ \s -> return (s,s)
+    put s = StateIO $ \_ -> return ((),s)
+
+
+instance (Monad m) => Monad (StateIO s m) where
+    return x = StateIO $ \s -> return (x, s)
+    (StateIO x) >>= f = StateIO $ 
+     \s -> do 
+        (out, s2) <- x s
+        (StateIO x') <- return $ f out
+        x' s2
+
+instance MonadTrans (StateIO s) where
+    lift c = StateIO $ \s -> c >>= (\x -> return (x,s))
+
+
 --instance Functor (MState s) where
 
 {-instance Monad (MState s) where
@@ -43,19 +71,21 @@ newtype MState s a = MState {runState2 :: s -> (a,s)}
                                       in g newState
 -}
 
-instance Functor (GameState) where 
+instance Functor (GameState s) where
     --fmap f (GameState g st) = GameState g newState
         --where newState = f st
     fmap = liftM
 
-instance Applicative (GameState) where
+instance Applicative (GameState s) where
     pure = return
     (<*>) = ap
 
 
-instance Monad GameState where
-    return = GameState
-    (GameState st) >>= f = f st
+instance Monad (GameState s) where
+    return x = GameState $ \s -> (x,s)
+    (GameState st) >>= f = GameState $ \s -> let (b, s2) = st s
+                                                 (GameState g) = f b
+                                            in   g s2
     --return x= GameState id
     --(GameState h) >>= f = GameState $ \s -> let (a,s2) =  h s
     --                                            (GameState g) = f a
@@ -74,7 +104,7 @@ instance Show (Grid Tile) where
 
 printBoard :: Grid Tile -> String
 printBoard (Grid g) = concatMap ((++"\n") . printRow) g
-    where 
+    where
         largestNum = length . show . maximum . catMaybes $ concat g
         printRow = unwords . map (padString . maybe "_" show)
         padding = flip subtract largestNum . length
@@ -96,7 +126,7 @@ updateGrid (Grid grid) (x, y) newData = Grid $ replace grid y newRow
 
 getEmptyTiles :: Grid Tile -> [Pos]
 getEmptyTiles (Grid grid) = fst <$> filteredGrid
-    where 
+    where
         numRows = length grid
         indexedGrid = zip [(x,y) | y <- [0..numRows-1], x <- [0..numRows-1]] $ concat grid
         filteredGrid = filter (isNothing . snd) indexedGrid
@@ -105,10 +135,12 @@ getEmptyTiles (Grid grid) = fst <$> filteredGrid
 createBoard :: Int -> Grid Tile
 createBoard =  (Grid .) . replicate <*> (`replicate` Nothing)
 
+
+
 shiftBoard :: Move -> Grid Tile -> Grid Tile
 shiftBoard R = shiftBoardRight
 shiftBoard m = rotateRight . shiftBoardRight . rotateLeft
-    where 
+    where
         rotations = getRotation m
         rotate dir g = iterate (rotateBoard dir) g !! rotations
         rotateLeft = rotate RLeft
@@ -130,8 +162,8 @@ shiftRowRight = (>>= (++)) <$> flip emptyTiles <*> getTiles
         -}
 
 shiftRowRight :: Tiles -> Tiles
-shiftRowRight row = emptyTiles ++ newTiles 
-    where 
+shiftRowRight row = emptyTiles ++ newTiles
+    where
         newTiles = map Just <$> collapseRow . catMaybes $ row
         emptyTiles = flip replicate Nothing $ (subtract `on` length) newTiles row
 
@@ -152,38 +184,56 @@ addNums' grid r = updateGrid grid newTile (pure 2)
 
 
 
-addNums :: (Grid Tile, StdGen)  -> (Grid Tile, StdGen)
-addNums (g, gen) = (addNums' g r, newGen)
-    where (r, newGen) = random gen
+--addNums :: int -> GameState (GState (Grid Tile)) (Grid Tile)
+addNums :: Int -> GState (Grid Tile) -> GState (Grid Tile)
+addNums 0 gs = gs
+addNums nums (GState grid gen) = addNums (nums-1) $ 
+  let (r, newGen) = random gen
+      newBoard = addNums' grid r
+                    in GState newBoard newGen
+
+liftGridFunc :: (a -> a) -> StateIO (GState a) IO (GState a)
+liftGridFunc f = do
+    gs <- get
+    return $ f <$> gs
+    
+
 
 --bind :: (Grid Tile -> Grid Tile) -> ()
-          
+
 
 main :: IO ()
-main = play . addNums . addNums $ newBoard
-        where newBoard = (createBoard 3, mkStdGen 100)
+main = do
+    y <- x newBoard
+    return ()
+        where newBoard = GState (createBoard 3) $ mkStdGen 100
+              play2 = play $ addNums 2 newBoard
+              x = runStateT play2
 
+checkGameOver :: GState (Grid Tile) -> StateIO (GState (Grid Tile)) IO ()
+checkGameOver gs@(GState grid _)
+    | length (getEmptyTiles grid) == 1 = lift $ putStrLn "Game Over"
+    | otherwise = play gs
 
-play :: (Grid Tile, StdGen) ->  IO ()
-play gg@(g, gen) 
-    | length (getEmptyTiles g) == 1 = putStrLn "Game Over"
-    | otherwise = do
-    putStrLn $ printBoard g
-    let (b1,g1) = addNums gg
-    putStrLn "------------"
-    putStr . printBoard $  b1
-    move <- getMove
-    case move of 
-        Nothing -> putStrLn "Game Over"
-        Just m -> play (shiftBoard m b1, g1)
+play :: GState (Grid Tile) -> StateIO (GState (Grid Tile)) IO ()
+play gs = do
+    lift $ putStrLn $ getVal $ printBoard <$> gs
+    put $ addNums 1 gs
+    lift $ putStrLn "------------" 
+    (GState a b) <- get
+    lift $ putStr $ printBoard a
+    move <- lift getMove
+    case move of
+        Nothing -> lift $ putStrLn "Game Over"
+        Just m ->  liftGridFunc  (shiftBoard m) >>= checkGameOver
 
 getMove :: IO (Maybe Move)
 getMove = do
     putStrLn "Enter W, D, S, A, or Q to quit."
-    inp <- getChar 
+    inp <- getChar
     putStrLn ""
-    maybe quit checkValid $ parseInput inp 
-    where 
+    maybe quit checkValid $ parseInput inp
+    where
         quit = return Nothing
         checkValid = maybe getMove (return . Just)
 
